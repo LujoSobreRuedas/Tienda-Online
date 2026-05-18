60e909b2f1307676433daa35b5b301a0981f2f63
franquil <sanchezfranquil93@gmail.com>
2026-05-18 18:41:27 -0500

    fix: mover requirements.txt a raiz y subir settings

--- TiendaWeb/settings.py
+++ TiendaWeb/settings.py
@@ -105,12 +105,18 @@ WSGI_APPLICATION = 'TiendaWeb.wsgi.application'
 # DATABASE
 # ============================================
 
+import os
+import dj_database_url
+
 DATABASES = {
     'default': dj_database_url.config(
-        default=os.environ.get('DATABASE_URL')
-    )
+        default=os.environ.get('DATABASE_URL'),
+        conn_max_age=600
+    ) if os.environ.get('DATABASE_URL') else {
+        'ENGINE': 'django.db.backends.sqlite3',
+        'NAME': BASE_DIR / 'db.sqlite3',
+    }
 }
-
 
 # ============================================
 # PASSWORD VALIDATION
